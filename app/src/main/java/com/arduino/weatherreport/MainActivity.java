a631ba4d1572324670163c9a35ebf1413ae51318
Masud Ahm <ma7332125@gmail.com>
2021-11-19 14:18:24 +0530

    initial commit

---
+++ app/src/main/java/com/arduino/weatherreport/MainActivity.java
@@ -0,0 +1,17 @@
+package com.arduino.weatherreport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Surface;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
